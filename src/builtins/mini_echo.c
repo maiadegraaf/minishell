944cd40776b2bcc01211471049a374f29096059a
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2022-03-16 10:01:54 +0100

    builtin change

--- src/builtins/mini_echo.c
+++ src/builtins/mini_echo.c
@@ -6,30 +6,30 @@
 /*   By: maiadegraaf <maiadegraaf@student.codam.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/16 10:47:40 by maiadegraaf   #+#    #+#                 */
-/*   Updated: 2022/03/15 12:53:23 by maiadegraaf   ########   odam.nl         */
+/*   Updated: 2022/03/15 14:50:27 by fpolycar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-void	print_lines(int i, char **str)
+void	print_lines(int i, char **str, int fd)
 {
 	while (str[i])
 	{
-		printf("%s", str[i++]);
+		ft_putstr_fd(str[i++], STDOUT_FILENO)
 		if (str[i])
-			printf(" ");
+			ft_putchar_fd(' ', STDOUT_FILENO);
 	}
 }
 
 int	mini_echo(t_tools *tools, t_simple_cmds *simple_cmd)
 {
 	if (!ft_strncmp(&tools->args[1], "-n", ft_strlen(&tools->args[1])))
-		print_lines(2, &tools->args);
+		print_lines(2, &tools->args, 1);
 	else
 	{
-		print_lines(1, &tools->args);
-		printf("\n");
+		print_lines(1, &tools->args, 1);
+		ft_putchar_fd('\n', STDOUT_FILENO);
 	}
 	return (1);
 }
--- src/builtins/mini_exit.c
+++ src/builtins/mini_exit.c
@@ -6,7 +6,7 @@
 /*   By: maiadegraaf <maiadegraaf@student.codam.      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/17 10:10:47 by maiadegraaf   #+#    #+#                 */
-/*   Updated: 2022/03/15 12:53:32 by maiadegraaf   ########   odam.nl         */
+/*   Updated: 2022/03/15 15:15:39 by fpolycar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 int	mini_exit(t_tools *tools, t_simple_cmds *simple_cmd)
 {
 	free(tools->args);
-	exit(0);
+	exit(STDERR_FILENO);
 	return (1);
 }
