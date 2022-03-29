a84b6d2f5487215dda027c53b4d56d9dc4387c4a
Maia de Graaf <maiadegraaf@gmail.com>
2022-03-29 15:05:28 +0200

    It works now?????

--- test/Testbuiltins.c
+++ test/Testbuiltins.c
@@ -25,39 +25,79 @@ void	init_test(char *line)
 	test_simple_cmd.redirections = NULL;
 }
 
-void	assert_mini_echo();
+void	assert_mini_echo(void)
+{
+	TEST_ASSERT_NOT_NULL_MESSAGE(test_simple_cmd.builtin, "Builtin function not connected.");
+}
+
+void	assert_mini_cd(char *expected_new_path, char *expected_old_path)
+{
+	TEST_ASSERT_NOT_NULL_MESSAGE(test_simple_cmd.builtin, "Builtin function not connected.");
+	TEST_ASSERT_EQUAL_STRING_MESSAGE(expected_new_path, tools->ne)
+}
+
+void	assert_mini_env(void)
+{
+	TEST_ASSERT_NOT_NULL_MESSAGE(test_simple_cmd.builtin, "Builtin function not connected.");
+}
+
+void	assert_mini_exit(void)
+{
+	TEST_ASSERT_NOT_NULL_MESSAGE(test_simple_cmd.builtin, "Builtin function not connected.");
+}
+
+void	assert_mini_export(void)
+{
+	TEST_ASSERT_NOT_NULL_MESSAGE(test_simple_cmd.builtin, "Builtin function not connected.");
+}
+
+void	assert_mini_pwd(void)
+{
+	TEST_ASSERT_NOT_NULL_MESSAGE(test_simple_cmd.builtin, "Builtin function not connected.");
+}
+
+void	assert_mini_unset(void)
+{
+	TEST_ASSERT_NOT_NULL_MESSAGE(test_simple_cmd.builtin, "Builtin function not connected.");
+}
 
 void test_builtin_1(void)
 {
 	init_test("echo hello");
-	
+	assert_mini_echo();
+}
+
+void test_builtin_2(void)
+{
+	init_test("echo hello");
+	assert_mini_echo();
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	UNITY_BEGIN();
-	// test_tools.envp = ft_arrdup(envp);
-	// RUN_TEST(test_builtin_1);
-	// TEST_MESSAGE("1: {test test}");
-	// RUN_TEST(test_builtin_2);
-	// TEST_MESSAGE("2: {test test | test}");
-	// RUN_TEST(test_builtin_3);
-	// TEST_MESSAGE("3: {test test | test      | \'test\'}");
-	// RUN_TEST(test_builtin_4);
-	// TEST_MESSAGE("4: {< redirection}");
-	// RUN_TEST(test_builtin_5);
-	// TEST_MESSAGE("5: {test > redirection}");
-	// RUN_TEST(test_builtin_6);
-	// TEST_MESSAGE("6: {test > redirection < redirection >> redirection}");
-	// RUN_TEST(test_builtin_7);
-	// TEST_MESSAGE("7: {test > redirection < redirection | heredoc << EOF}");
-	// RUN_TEST(test_builtin_8);
-	// TEST_MESSAGE("8: {test > redirection < redirection | heredoc < redirection >> redirection << EOF}");
-	// RUN_TEST(test_builtin_9);
-	// TEST_MESSAGE("9: {cat test | test < redirection | echo test}");
-	// RUN_TEST(test_builtin_10);
-	// TEST_MESSAGE("10: {cd test | env < redirection | exit test}");
-	// RUN_TEST(test_builtin_11);
-	// TEST_MESSAGE("11: {export test | pwd < redirection | unset test}");
+	test_tools.envp = ft_arrdup(envp);
+	RUN_TEST(test_builtin_1);
+	TEST_MESSAGE("1: {echo hello}");
+	RUN_TEST(test_builtin_2);
+	TEST_MESSAGE("2: {test test | test}");
+	RUN_TEST(test_builtin_3);
+	TEST_MESSAGE("3: {test test | test      | \'test\'}");
+	RUN_TEST(test_builtin_4);
+	TEST_MESSAGE("4: {< redirection}");
+	RUN_TEST(test_builtin_5);
+	TEST_MESSAGE("5: {test > redirection}");
+	RUN_TEST(test_builtin_6);
+	TEST_MESSAGE("6: {test > redirection < redirection >> redirection}");
+	RUN_TEST(test_builtin_7);
+	TEST_MESSAGE("7: {test > redirection < redirection | heredoc << EOF}");
+	RUN_TEST(test_builtin_8);
+	TEST_MESSAGE("8: {test > redirection < redirection | heredoc < redirection >> redirection << EOF}");
+	RUN_TEST(test_builtin_9);
+	TEST_MESSAGE("9: {cat test | test < redirection | echo test}");
+	RUN_TEST(test_builtin_10);
+	TEST_MESSAGE("10: {cd test | env < redirection | exit test}");
+	RUN_TEST(test_builtin_11);
+	TEST_MESSAGE("11: {export test | pwd < redirection | unset test}");
 	return UNITY_END();
 }
