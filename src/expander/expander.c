ecc86a6e469f24d6509e4972d1a0bd03ac875399
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2022-03-31 16:23:39 +0200

    expander done

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,162 +6,84 @@
 /*   By: fpolycar <fpolycar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/15 13:35:26 by fpolycar      #+#    #+#                 */
-/*   Updated: 2022/03/30 16:20:30 by fpolycar      ########   odam.nl         */
+/*   Updated: 2022/03/31 16:22:13 by fpolycar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "builtins.h"
 
-size_t	dollar_sign(char *str)
+int	loop_if_dollar_sign(t_tools *tools, char *arr_tmp, int j, int i)
 {
-	size_t	i;
+	int		k;
+	int		ret;
+	char	*tmp;
+	char	*tmp2;
 
-	i = 0;
-	while (str[i])
+	k = 0;
+	ret = 0;
+	while (tools->envp[k])
 	{
-		if (str[i] == '$')
-			return (i + 1);
-		i++;
+		if (ft_strncmp(arr_tmp + j + 1,
+				tools->envp[k], equal_sign(tools->envp[k]) - 1) == 0
+			&& after_dollar_lenght(arr_tmp, j) - j
+			== equal_sign(tools->envp[k]))
+		{
+			tmp2 = ft_strdup(tools->envp[k] + equal_sign(tools->envp[k]));
+			tmp = ft_strjoin(tools->simple_cmds->str[i], tmp2);
+			free_things(tmp2, tools, i);
+			tools->simple_cmds->str[i] = tmp;
+			ret = equal_sign(tools->envp[k]);
+		}
+		k++;
 	}
-	return (0);
+	if (ret == 0)
+		ret = after_dollar_lenght(arr_tmp, j) - j;
+	return (ret);
 }
 
-int	arg_length(char **envp)
-{
-	int	i;
-
-	i = 0;
-	while (envp[i])
-		i++;
-	return (i);
-}
-
-	
-
-// }
-
-// size_t str_replace_with_var_lenght(char *str, t_tools *tools, t_simple_cmds *tmp)
-// {
-// 	size_t	len;
-// 	int		i;
-// 	int		k;
-// 	int		process;
-
-// 	len = 0;
-// 	i = 0;
-// 	process = 0;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '$')
-// 		{
-// 			k = 0;
-// 			while (tools->envp[k])
-// 			{
-// 				if (ft_strncmp(str + i + 1,
-// 						tools->envp[k], equal_sign(tools->envp[k]) - 1) == 0
-// 					&& (str[equal_sign(tools->envp[k]) + i] == '\0'
-// 						|| str[equal_sign(tools->envp[k]) + i] == '$'
-// 						|| str[equal_sign(tools->envp[k]) + i] == ' '
-// 						|| str[equal_sign(tools->envp[k]) + i] == '\"'))
-// 				{
-// 					// len += ft_strlen(tools->envp[k] + equal_sign(tools->envp[k]));
-// 					i += equal_sign(tools->envp[k]);
-// 					process = 1;
-// 				}
-// 				if (process == 0 && k == arg_length(tools->envp) - 1)
-// 					i += ft_strlen(str + i);
-// 				k++;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			len++;
-// 			i++;
-// 		}
-// 	}
-// 	return(len);
-// }
-
-void	expander_loop(t_tools *tools, t_simple_cmds *tmp, int i)
+char	*detect_dollar_sign(t_tools *tools, char *str, int i)
 {
 	int		j;
-	int		k;
-	int		len_non_var;
 	char	*arr_tmp;
-	char	*tmp_transformed = NULL;
-	char	*tmp_tmp;
+	char	*tmp;
+	char	*tmp2;
 
 	j = 0;
-	arr_tmp = tmp->str[i];
+	arr_tmp = ft_strdup(str);
 	while (arr_tmp[j])
 	{
+		init_stri(i, j, tools);
 		if (arr_tmp[j] == '$')
-		{
-			k = 0;
-			while (tools->envp[k])
-			{
-				if (ft_strncmp(arr_tmp + j + 1,
-						tools->envp[k], equal_sign(tools->envp[k]) - 1) == 0
-					&& (arr_tmp[equal_sign(tools->envp[k]) + j] == '\0'
-						|| arr_tmp[equal_sign(tools->envp[k]) + j] == '$'
-						|| arr_tmp[equal_sign(tools->envp[k]) + j] == ' '
-						|| arr_tmp[equal_sign(tools->envp[k]) + j] == '\"'))
-				{
-					if (!tmp_transformed)
-						tmp_transformed = ft_substr(tools->envp[k],
-								equal_sign(tools->envp[k]),
-								ft_strlen(tools->envp[k]));
-					else
-					{
-						tmp_tmp = tmp_transformed;
-						free(tmp_transformed);
-						tmp_transformed = ft_strjoin(tmp_tmp, ft_substr(tools->envp[k],
-								equal_sign(tools->envp[k]),
-								ft_strlen(tools->envp[k])));	
-					}
-					j += equal_sign(tools->envp[k]);
-				}
-				k++;
-			}
-		}
+			j += loop_if_dollar_sign(tools, arr_tmp, j, i);
 		else
 		{
-			if (!tmp_transformed)
-			{
-				tmp_transformed = ft_strdup(arr_tmp);
-				ft_strlcpy(tmp_transformed, tmp_transformed, 2);
-			}
-			else
-			{
-				tmp_tmp = tmp_transformed;
-				free(tmp_transformed);
-				tmp_transformed = ft_strjoin(tmp_transformed, tmp_tmp);	
-			}
+			tmp2 = char_to_str(arr_tmp[j]);
+			tmp = ft_strjoin(tools->simple_cmds->str[i], tmp2);
+			free_things(tmp2, tools, i);
+			tools->simple_cmds->str[i] = tmp;
+			str = tmp;
 			j++;
-		printf("tmp_transformed = %s\n\n", tmp_transformed);
 		}
 	}
-	// free(tmp->str[i]);
-	// tmp->str[i] = tmp_transformed;
+	free(arr_tmp);
+	return (tools->simple_cmds->str[i]);
 }
 
-void	expander(t_tools *tools, t_simple_cmds *simple_cmds)
+void	expander(t_tools *tools)
 {
 	t_simple_cmds	*tmp;
 	int				i;
 
 	i = 0;
-	tmp = simple_cmds;
+	tmp = tools->simple_cmds;
 	while (tmp)
 	{
-		while (tmp->str[i] && dollar_sign(tmp->str[i]) != 0)
+		while (tmp->str[i])
 		{
-			if (tmp->str[i][0] != '\'' && tmp->str[i][ft_strlen(tmp->str[i])] != '\'')
-			{
-				// printf("%zu\n", str_replace_with_var_lenght(tmp->str[i], tools, tmp));
-				expander_loop(tools, tmp, i);
-			}
+			if (tmp->str[i][0] != '\''
+				&& tmp->str[i][ft_strlen(tmp->str[i])] != '\'')
+				detect_dollar_sign(tools, tmp->str[i], i);
 			i++;
 		}
 		tmp = tmp->next;
---
+++ src/expander/expanders_utils.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   expanders_utils.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fpolycar <fpolycar@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/03/31 16:08:47 by fpolycar      #+#    #+#                 */
+/*   Updated: 2022/03/31 16:22:46 by fpolycar      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "builtins.h"
+
+void	init_stri(int i, int j, t_tools *tools)
+{
+	if (j == 0)
+	{
+		free(tools->simple_cmds->str[i]);
+		tools->simple_cmds->str[i] = ft_strdup("\0");
+	}
+}
+
+void	free_things(char *tmp2, t_tools *tools, int i)
+{
+	free(tmp2);
+	free(tools->simple_cmds->str[i]);
+}
+
+size_t	dollar_sign(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+			return (i + 1);
+		i++;
+	}
+	return (0);
+}
+
+char	*char_to_str(char c)
+{
+	char	*str;
+
+	str = ft_calloc(sizeof(char), 2);
+	str[0] = c;
+	return (str);
+}
+
+int	after_dollar_lenght(char *str, int j)
+{
+	int	i;
+
+	i = j + 1;
+	while (str[i] != '\0' && str[i] != '$' && str[i] != ' '
+		&& str[i] != '\"')
+		i++;
+	return (i);
+}
